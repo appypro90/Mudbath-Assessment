ab967c7a7c07493628d25e6792ff80a1d7c63714
Arpita Sen <Arpita Sen@LAPTOP-LIGVQST5>
2021-04-08 19:19:50 +1000

    Refactoring code

---
+++ backend/Dal/Helpers/FileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Dal.Helpers
+{
+    public class FileReader
+    {
+        public static string LoadJsonFile(string fileName)
+        {
+            var path = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            var actualPath = path.Substring(0, path.LastIndexOf("bin", StringComparison.Ordinal));
+            var projectPath = new Uri(actualPath).LocalPath;
+            var filePath = projectPath + @fileName;
+            string json;
+            json = ReadJsonFile(filePath);
+            return json;
+        }
+
+        public static string ReadJsonFile(string fileName)
+        {
+            string json;
+            using (var r = new StreamReader(fileName))
+            {
+                json = r.ReadToEnd();
+            }
+            return json;
+        }
+    }
+}
